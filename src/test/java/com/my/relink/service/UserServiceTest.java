2fed6b89610ba307a9192ac54cf48b46cf8714be
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-07 23:15:38 +0900

    :safety_vest: [#42] test(UserServiceTest) : UserService 테스트 코드 추가

---
+++ src/test/java/com/my/relink/service/UserServiceTest.java
@@ -0,0 +1,71 @@
+package com.my.relink.service;
+
+import com.my.relink.domain.user.Address;
+import com.my.relink.domain.user.User;
+import com.my.relink.domain.user.UserRepository;
+import com.my.relink.dto.req.UserCreateReqDto;
+import com.my.relink.dto.resp.UserCreateRespDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("정상적인 회원가입 성공")
+    void registerSuccess() {
+
+        // given
+        UserCreateReqDto reqDto = UserCreateReqDto.builder()
+                .name("test")
+                .email("test@example.com")
+                .nickname("test")
+                .contact("010-1111-1111")
+                .password("password1234")
+                .address(new Address(12345, "test", "test"))
+                .build();
+        User user = reqDto.toEntity(reqDto);
+
+        String encodedPassword = "encodedPassword";
+        when(passwordEncoder.encode(any())).thenReturn(encodedPassword);
+
+        User savedUser = User.builder()
+                .id(1L)
+                .email(user.getEmail())
+                .password(encodedPassword)
+                .name(user.getName())
+                .nickname(user.getNickname())
+                .contact(user.getContact())
+                .address(user.getAddress())
+                .build();
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+        // when
+        UserCreateRespDto register = userService.register(reqDto);
+
+        // then
+        assertThat(register.userId()).isEqualTo(1L);
+        verify(userRepository).save(any());
+        verify(passwordEncoder).encode(any());
+    }
+}
