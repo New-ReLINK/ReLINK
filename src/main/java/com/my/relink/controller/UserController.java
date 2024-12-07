a67302efebe179f19ca7c82aaa548e9a5676b890
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-07 23:13:07 +0900

    :sparkles: [#42] feat(UserController) : 회원가입 컨트롤러 구현

--- src/main/java/com/my/relink/controller/UserController.java
+++ src/main/java/com/my/relink/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.my.relink.controller;
 
+import com.my.relink.dto.req.UserCreateReqDto;
+import com.my.relink.dto.resp.UserCreateRespDto;
+import com.my.relink.service.UserService;
+import com.my.relink.util.api.ApiResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/signup")
+    public ResponseEntity<ApiResult<UserCreateRespDto>> signup(@Valid @RequestBody UserCreateReqDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.success(userService.register(dto)));
+    }
 }
