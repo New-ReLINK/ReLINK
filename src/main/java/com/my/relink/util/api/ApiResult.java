770a262621fae46919ff2f86b91c86f50f10673d
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-07 16:37:51 +0900

    feat: 공통 api 응답 포맷 추가 #37

--- src/main/java/com/my/relink/util/api/ApiResult.java
+++ src/main/java/com/my/relink/util/api/ApiResult.java
@@ -1,7 +1,26 @@
 package com.my.relink.util.api;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public class ApiResult {
+@AllArgsConstructor
+@JsonPropertyOrder({"success", "data", "error"})
+public class ApiResult<T> {
+    private final T data;
+    private final boolean success;
+    private final ApiError error;
+
+    public static <T>ApiResult<T> success(T data){
+        return new ApiResult<>(data, true, null);
+    }
+
+    public static <T>ApiResult<T> error(String message, int status){
+        return new ApiResult<>(null, false, new ApiError(message, status));
+    }
+
+    public static <T>ApiResult<T> error(T errorData, String message, int status){
+        return new ApiResult<>(errorData, false, new ApiError(message, status));
+    }
 }
