275daa53a43f8f7e29809dd484fff961b4bef1e8
siyeon02 <chrysapark@cau.ac.kr>
2024-12-07 22:58:56 +0900

    feat(api-result) : apiresult 형식 #11 - apiresult 형식 - apierror 형식

--- src/main/java/com/my/relink/util/api/ApiError.java
+++ src/main/java/com/my/relink/util/api/ApiError.java
@@ -1,8 +1,16 @@
 package com.my.relink.util.api;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 
 @Getter
 public class ApiError {
+    private final String msg;
+    private final int status;
+
+    public ApiError(String msg, int status) {
+        this.msg = msg;
+        this.status = status;
+    }
 
 }
