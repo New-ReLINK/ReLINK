8f44cd913d08f9189c8a3aaf87af00a3324c83f1
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-07 23:11:21 +0900

    :sparkles: [#42] feat(UserService) : 회원가입 서비스 로직 구현

--- src/main/java/com/my/relink/service/UserService.java
+++ src/main/java/com/my/relink/service/UserService.java
@@ -1,4 +1,23 @@
 package com.my.relink.service;
 
+import com.my.relink.domain.user.User;
+import com.my.relink.domain.user.UserRepository;
+import com.my.relink.dto.req.UserCreateReqDto;
+import com.my.relink.dto.resp.UserCreateRespDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserCreateRespDto register(UserCreateReqDto dto) {
+        dto.changePassword(passwordEncoder.encode(dto.getPassword()));
+        User savedUser = userRepository.save(dto.toEntity(dto));
+        return new UserCreateRespDto(savedUser.getId());
+    }
 }
