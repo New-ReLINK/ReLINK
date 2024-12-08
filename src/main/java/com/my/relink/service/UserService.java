e28162d75b82a90f72fb92207889f1a20a68e714
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-08 19:21:32 +0900

    :sparkles: [#43] feat(UserService) : user 정보를 찾는 메서드 구현

--- src/main/java/com/my/relink/service/UserService.java
+++ src/main/java/com/my/relink/service/UserService.java
@@ -1,9 +1,15 @@
 package com.my.relink.service;
 
+import com.my.relink.domain.image.EntityType;
+import com.my.relink.domain.image.Image;
+import com.my.relink.domain.image.ImageRepository;
 import com.my.relink.domain.user.User;
 import com.my.relink.domain.user.UserRepository;
 import com.my.relink.dto.req.UserCreateReqDto;
 import com.my.relink.dto.resp.UserCreateRespDto;
+import com.my.relink.dto.resp.UserInfoRespDto;
+import com.my.relink.ex.BusinessException;
+import com.my.relink.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,10 +20,20 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ImageRepository imageRepository;
 
     public UserCreateRespDto register(UserCreateReqDto dto) {
         dto.changePassword(passwordEncoder.encode(dto.getPassword()));
         User savedUser = userRepository.save(dto.toEntity(dto));
         return new UserCreateRespDto(savedUser.getId());
+    }
+
+    public UserInfoRespDto findUserInfo(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Image image = imageRepository.findByEntityIdAndEntityType(user.getId(), EntityType.USER).orElse(null);
+
+        return new UserInfoRespDto(user, image);
     }
 }
