82cb8a3ab158813c737919ea60b8aacf8dcc58c7
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-08 17:33:55 +0900

    :recycle: [#38] feat(LoginReqDto) : 로그인 요청 dto 구현

---
+++ src/main/java/com/my/relink/config/security/dto/LoginRepDto.java
@@ -0,0 +1,21 @@
+package com.my.relink.config.security.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class LoginRepDto {
+
+    @NotBlank(message = "이메일을 적어주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "패스워드를 적어주세요.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,20}$",
+            message = "비밀번호는 영문 대소문자와 숫자를 포함해서 8~20자리여야 합니다"
+    )
+    private String password;
+}
