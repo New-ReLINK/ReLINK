9a48f3c09758613ebdad9c0de063b430c4f6e34c
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-08 19:20:23 +0900

    :sparkles: [#43] feat(UserInfoRespDto) : user 정보 조회 dto 구현

---
+++ src/main/java/com/my/relink/dto/resp/UserInfoRespDto.java
@@ -0,0 +1,19 @@
+package com.my.relink.dto.resp;
+
+import com.my.relink.domain.image.Image;
+import com.my.relink.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class UserInfoRespDto {
+
+    private String name;
+    private String nickname;
+    private String imageUrl;
+
+    public UserInfoRespDto(User user, Image image) {
+        this.name = user.getName();
+        this.nickname = user.getNickname();
+        this.imageUrl = image != null ? image.getImageUrl() : null;
+    }
+}
