648dbd340bf9f457540d7b7ffe76acf69b389902
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-08 17:38:03 +0900

    :sparkles: [#38] feat(AuthUser) : 토큰에서 꺼낸 유저정보를 담을 객체 구현

---
+++ src/main/java/com/my/relink/config/security/AuthUser.java
@@ -0,0 +1,26 @@
+package com.my.relink.config.security;
+
+import com.my.relink.domain.user.Role;
+import com.my.relink.domain.user.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class AuthUser {
+
+    private final String email;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public AuthUser(String email, Role role) {
+        this.email = email;
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    public static AuthUser from(User user) {
+        return new AuthUser(user.getEmail(), user.getRole());
+    }
+}
