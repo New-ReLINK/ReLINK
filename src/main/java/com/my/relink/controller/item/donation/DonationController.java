8f9bcb53434739c6b3aeb2404cbc4e314d405fbf
GoUiJeong <uijeong0525@gmail.com>
2024-12-09 20:17:02 +0900

    feat(donationItem-controller) : DonationItemController 추가

---
+++ src/main/java/com/my/relink/controller/item/donation/DonationController.java
@@ -0,0 +1,25 @@
+package com.my.relink.controller.item.donation;
+
+import com.my.relink.config.security.AuthUser;
+import com.my.relink.controller.item.donation.dto.DonationItemReqDto;
+import com.my.relink.controller.item.donation.dto.DonationItemRespDto;
+import com.my.relink.service.DonationItemService;
+import com.my.relink.util.api.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class DonationController {
+    private final DonationItemService donationItemService;
+
+    @PostMapping("/item/donation")
+    public ResponseEntity<ApiResult<DonationItemRespDto>> createDonationItem(@AuthenticationPrincipal AuthUser authUser, @RequestBody DonationItemReqDto request) {
+        DonationItemRespDto response = donationItemService.createDonationItem(request, authUser);
+        return new ResponseEntity<>(ApiResult.success(response), HttpStatus.OK);
+    }
+
+}
