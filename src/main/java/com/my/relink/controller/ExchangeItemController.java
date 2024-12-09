441f02d92b7543300cf5d1521683d72ac12a2909
TIP_Santa <bhb89@naver.com>
2024-12-09 16:41:15 +0900

    Refactor: ExchangeItemController - 응답값 형식에 맞춰 수정

--- src/main/java/com/my/relink/controller/ExchangeItemController.java
+++ src/main/java/com/my/relink/controller/ExchangeItemController.java
@@ -2,7 +2,9 @@ package com.my.relink.controller;
 
 import com.my.relink.domain.item.exchange.dto.CreateExchangeItemReqDto;
 import com.my.relink.service.ExchangeItemService;
+import com.my.relink.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +17,7 @@ public class ExchangeItemController {
     private final ExchangeItemService exchangeItemService;
 
     @PostMapping("item/exchange")
-    public ResponseEntity<Long> createExchangeItem(@RequestBody CreateExchangeItemReqDto reqDto) {
-        return ResponseEntity.ok(exchangeItemService.createExchangeItem(reqDto));
+    public ResponseEntity<ApiResult<Long>> createExchangeItem(@RequestBody CreateExchangeItemReqDto reqDto) {
+        return new ResponseEntity<>(ApiResult.success(exchangeItemService.createExchangeItem(reqDto)), HttpStatus.OK);
     }
 }
