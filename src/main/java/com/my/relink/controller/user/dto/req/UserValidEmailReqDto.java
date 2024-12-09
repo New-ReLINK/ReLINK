d5765c6190fae7d3b9f8940b50871553205b0f56
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-09 21:22:51 +0900

    :sparkles: [#46] test(UserValidEmailReqDto) : 테스트코드를 위한 Build 패턴 추가

--- src/main/java/com/my/relink/controller/user/dto/req/UserValidEmailReqDto.java
+++ src/main/java/com/my/relink/controller/user/dto/req/UserValidEmailReqDto.java
@@ -2,12 +2,16 @@ package com.my.relink.controller.user.dto.req;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserValidEmailReqDto {
 
     @NotBlank(message = "이메일을 적어주세요.")
