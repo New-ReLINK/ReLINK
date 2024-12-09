4e65e348e77c61e080aae20909a20ee3a8976910
siyeon02 <chrysapark@cau.ac.kr>
2024-12-09 20:37:08 +0900

    fix(trade-controller) : dto명 변경 #12

--- src/main/java/com/my/relink/controller/TradeController.java
+++ src/main/java/com/my/relink/controller/TradeController.java
@@ -1,7 +1,7 @@
 package com.my.relink.controller;
 
-import com.my.relink.domain.trade.dto.TradeRequestDto;
-import com.my.relink.domain.trade.dto.TradeRequestResponseDto;
+import com.my.relink.controller.trade.dto.request.TradeReqDto;
+import com.my.relink.controller.trade.dto.response.TradeRequestRespDto;
 import com.my.relink.service.TradeService;
 import com.my.relink.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +18,13 @@ public class TradeController {
     private final TradeService tradeService;
 
     @PostMapping("/trades/{tradeId}/request")
-    public ResponseEntity<ApiResult<TradeRequestResponseDto>> requestTrade(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeRequestDto tradeRequestDto) {//추후 로그인 유저로 바뀔 얘정
-        TradeRequestResponseDto responseDto = tradeService.requestTrade(tradeId, tradeRequestDto.getUserId());
+    public ResponseEntity<ApiResult<TradeRequestRespDto>> requestTrade(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeReqDto tradeRequestDto) {//추후 로그인 유저로 바뀔 얘정
+        TradeRequestRespDto responseDto = tradeService.requestTrade(tradeId, tradeRequestDto.getUserId());
         return new ResponseEntity<>(ApiResult.success(responseDto), HttpStatus.OK);
     }
 
     @PostMapping("/trades/{tradeId}/request-cancel")
-    public ResponseEntity<Void> cancelTradeRequest(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeRequestDto tradeRequestDto) {
+    public ResponseEntity<Void> cancelTradeRequest(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeReqDto tradeRequestDto) {
         tradeService.cancelTradeRequest(tradeId, tradeRequestDto.getUserId()); // 서비스에서 취소 로직 처리
         return ResponseEntity.noContent().build(); // 본문 없이 204 No Content 응답 반환
     }
