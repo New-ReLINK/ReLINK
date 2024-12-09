170002f9011a9720b593733727715fe71590f203
siyeon02 <chrysapark@cau.ac.kr>
2024-12-09 20:20:21 +0900

    refactor(repository) : 레포지토리 디렉토리 변경 #14

---
+++ src/main/java/com/my/relink/controller/trade/dto/request/TradeReqDto.java
@@ -0,0 +1,8 @@
+package com.my.relink.controller.trade.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class TradeReqDto {
+    Long userId;
+}
--- src/main/java/com/my/relink/domain/point/pointHistory/repository/PointHistoryRepository.java
+++ src/main/java/com/my/relink/domain/point/pointHistory/repository/PointHistoryRepository.java
@@ -4,7 +4,6 @@ import com.my.relink.domain.point.pointHistory.PointHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
