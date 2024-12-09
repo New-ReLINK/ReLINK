cc2941944eb2623a841d98acf7d073d8187830e1
TIP_Santa <bhb89@naver.com>
2024-12-09 20:53:43 +0900

    #19 Refactor -. ExchangeItemController : AuthUser를 통하여 토큰에서 userId 추출

--- src/main/java/com/my/relink/controller/exchangeItem/ExchangeItemController.java
+++ src/main/java/com/my/relink/controller/exchangeItem/ExchangeItemController.java
@@ -1,13 +1,14 @@
 package com.my.relink.controller.exchangeItem;
 
+import com.my.relink.config.security.AuthUser;
 import com.my.relink.controller.exchangeItem.dto.CreateExchangeItemReqDto;
 import com.my.relink.service.ExchangeItemService;
 import com.my.relink.util.api.ApiResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,8 @@ public class ExchangeItemController {
     private final ExchangeItemService exchangeItemService;
 
     @PostMapping("/item/exchange")
-    public ResponseEntity<ApiResult<Long>> createExchangeItem(@RequestBody CreateExchangeItemReqDto reqDto) {
-        // 토큰에서 인증된 사용자 정보 가져오기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Long userId = (Long) authentication.getPrincipal();
-        Long exchangeItemId = exchangeItemService.createExchangeItem(reqDto, userId);
+    public ResponseEntity<ApiResult<Long>> createExchangeItem(@Valid @RequestBody CreateExchangeItemReqDto reqDto, @AuthenticationPrincipal AuthUser authUser) {
+        Long exchangeItemId = exchangeItemService.createExchangeItem(reqDto, authUser.getId());
         return new ResponseEntity<>(ApiResult.success(exchangeItemId), HttpStatus.OK);
     }
 }
