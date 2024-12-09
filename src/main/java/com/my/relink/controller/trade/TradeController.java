f93a5558432255c495c599584d3f919f20218b28
siyeon02 <chrysapark@cau.ac.kr>
2024-12-09 23:14:55 +0900

    fix(trade-controller) : 시큐리티 적용 #68

--- src/main/java/com/my/relink/controller/trade/TradeController.java
+++ src/main/java/com/my/relink/controller/trade/TradeController.java
@@ -1,5 +1,6 @@
-package com.my.relink.controller;
+package com.my.relink.controller.trade;
 
+import com.my.relink.config.security.AuthUser;
 import com.my.relink.controller.trade.dto.request.TradeReqDto;
 import com.my.relink.controller.trade.dto.response.TradeRequestRespDto;
 import com.my.relink.service.TradeService;
@@ -7,6 +8,7 @@ import com.my.relink.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,15 +20,15 @@ public class TradeController {
     private final TradeService tradeService;
 
     @PostMapping("/trades/{tradeId}/request")
-    public ResponseEntity<ApiResult<TradeRequestRespDto>> requestTrade(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeReqDto tradeRequestDto) {//추후 로그인 유저로 바뀔 얘정
-        TradeRequestRespDto responseDto = tradeService.requestTrade(tradeId, tradeRequestDto.getUserId());
+    public ResponseEntity<ApiResult<TradeRequestRespDto>> requestTrade(@PathVariable(name = "tradeId") Long tradeId, @AuthenticationPrincipal AuthUser authUser) {//추후 로그인 유저로 바뀔 얘정
+        TradeRequestRespDto responseDto = tradeService.requestTrade(tradeId, authUser);
         return new ResponseEntity<>(ApiResult.success(responseDto), HttpStatus.OK);
     }
 
     @PostMapping("/trades/{tradeId}/request-cancel")
-    public ResponseEntity<Void> cancelTradeRequest(@PathVariable(name = "tradeId") Long tradeId, @RequestBody TradeReqDto tradeRequestDto) {
-        tradeService.cancelTradeRequest(tradeId, tradeRequestDto.getUserId()); // 서비스에서 취소 로직 처리
-        return ResponseEntity.noContent().build(); // 본문 없이 204 No Content 응답 반환
+    public ResponseEntity<Void> cancelTradeRequest(@PathVariable(name = "tradeId") Long tradeId, @AuthenticationPrincipal AuthUser authUser) {
+        tradeService.cancelTradeRequest(tradeId, authUser);
+        return ResponseEntity.noContent().build();
     }
 
 }
