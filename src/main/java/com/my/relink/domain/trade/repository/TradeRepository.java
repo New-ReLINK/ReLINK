50d2e2757b1eb80db839e36acef32e7e337123f3
siyeon02 <chrysapark@cau.ac.kr>
2024-12-09 10:55:18 +0900

    fix(trade-repository) : trade repository 어노테이션 추가 #11

--- src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
+++ src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
@@ -2,6 +2,8 @@ package com.my.relink.domain.trade.repository;
 
 import com.my.relink.domain.trade.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 }
--- src/main/java/com/my/relink/domain/user/repository/UserRepository.java
+++ src/main/java/com/my/relink/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.my.relink.domain.user.repository;
 
 import com.my.relink.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
