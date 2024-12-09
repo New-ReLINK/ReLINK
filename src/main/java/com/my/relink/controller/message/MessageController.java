625984378cf045410734bff29b390bf0d22c6baa
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-09 22:20:30 +0900

    feat(message-controller): 채팅 메시지 내역 조회 시 사용하는 size 쿼리 파라미터에 valid 추가 #34

--- src/main/java/com/my/relink/controller/message/MessageController.java
+++ src/main/java/com/my/relink/controller/message/MessageController.java
@@ -3,8 +3,11 @@ package com.my.relink.controller.message;
 import com.my.relink.controller.message.dto.response.MessageRespDto;
 import com.my.relink.service.MessageService;
 import com.my.relink.util.api.ApiResult;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class MessageController {
 
     private final MessageService messageService;
@@ -19,7 +23,7 @@ public class MessageController {
     @GetMapping("/chat/{tradeId}/messages")
     public ResponseEntity<ApiResult<MessageRespDto>> getChatRoomMessages(
             @PathVariable("tradeId") Long tradeId,
-            @RequestParam(value = "size", required = false, defaultValue = "10") int size,
+            @RequestParam(value = "size", required = false, defaultValue = "10") @Min(1) @Max(100) int size,
             @RequestParam(value = "cursor", required = false) Long cursor) {
         return ResponseEntity.ok(ApiResult.success(messageService.getChatRoomMessage(tradeId, size, cursor)));
     }
