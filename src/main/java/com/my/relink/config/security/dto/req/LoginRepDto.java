7f91370d514655e73c6089d705d8ba32166cb73f
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-09 11:43:12 +0900

    :package: [#38] package(LoginRepDto) : 패키지 구조 변경

--- src/main/java/com/my/relink/config/security/dto/req/LoginRepDto.java
+++ src/main/java/com/my/relink/config/security/dto/req/LoginRepDto.java
@@ -1,4 +1,4 @@
-package com.my.relink.config.security.dto;
+package com.my.relink.config.security.dto.req;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
