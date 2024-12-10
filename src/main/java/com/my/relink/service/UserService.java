929a890f4c49d3a8d1aac1773fc3262e61f8598e
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-10 18:30:25 +0900

    :package: [#55] package : 패키지 구조 변경

--- src/main/java/com/my/relink/service/UserService.java
+++ src/main/java/com/my/relink/service/UserService.java
@@ -1,14 +1,13 @@
 package com.my.relink.service;
 
-import com.my.relink.domain.image.EntityType;
-import com.my.relink.domain.image.Image;
-import com.my.relink.domain.image.ImageRepository;
-import com.my.relink.domain.review.ReviewRepository;
-import com.my.relink.domain.user.User;
-import com.my.relink.domain.user.repository.UserRepository;
 import com.my.relink.controller.user.dto.req.UserCreateReqDto;
 import com.my.relink.controller.user.dto.resp.UserCreateRespDto;
 import com.my.relink.controller.user.dto.resp.UserInfoRespDto;
+import com.my.relink.domain.image.EntityType;
+import com.my.relink.domain.image.Image;
+import com.my.relink.domain.image.ImageRepository;
+import com.my.relink.domain.user.User;
+import com.my.relink.domain.user.repository.UserRepository;
 import com.my.relink.ex.BusinessException;
 import com.my.relink.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
