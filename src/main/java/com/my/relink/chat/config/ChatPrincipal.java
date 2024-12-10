ab26369330593b6c3cd9beb4c21db0cf7646c352
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-10 17:14:27 +0900

    feat(websocket-config): 웹소켓 설정 추가 #35

---
+++ src/main/java/com/my/relink/chat/config/ChatPrincipal.java
@@ -0,0 +1,2 @@
+package com.my.relink.chat.config;public class ChatPrincipal {
+}
--- src/main/java/com/my/relink/chat/config/WebSocketConfig.java
+++ src/main/java/com/my/relink/chat/config/WebSocketConfig.java
@@ -1,19 +1,51 @@
 package com.my.relink.chat.config;
 
+import com.my.relink.chat.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker //STOMP 활성화
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final StompHandler stompHandler;
+
+    /**
+     * 메시지 브로커 설정
+     * @param registry
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub"); //구독하는 쪽
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic"); //구독 요청을 처리할 prefix
+        registry.setApplicationDestinationPrefixes("/app"); //클라이언트가 메시지를 발행할 때 사용할 prefix
+    }
+
+    /**
+     * 웹소켓 엔드포인트 등록 및 모든 오리진에서의 접근 허용
+     *
+     * ex) ws:localhost:9090/chats
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chats")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+
+    /**
+     * 클라이언트로부터 들어오는 메시지를 처리하는 채널에 인터셉터 등록
+     * @param registration
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
