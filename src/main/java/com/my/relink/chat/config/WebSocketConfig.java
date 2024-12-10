3d1a3ea1a3261ee9d47700112047f536b9525ed6
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-10 15:43:46 +0900

    feat(websocket-config): 웹소켓 설정 추가중 #35

---
+++ src/main/java/com/my/relink/chat/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.my.relink.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); //구독하는 쪽
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
