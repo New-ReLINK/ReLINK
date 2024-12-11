d77ad97e6d51eaf9e89e31c8eff7750cc172fdd3
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-11 14:23:33 +0900

    feat(message-repo): Message 레퍼지토리 추가 #83

--- src/main/java/com/my/relink/domain/message/repository/MessageRepository.java
+++ src/main/java/com/my/relink/domain/message/repository/MessageRepository.java
@@ -1,2 +1,7 @@
-package com.my.relink.domain.message.repository;public interface MessageRepository {
+package com.my.relink.domain.message.repository;
+
+import com.my.relink.domain.message.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
