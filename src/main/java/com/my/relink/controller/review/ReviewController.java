4451d90fb005c0aadf4ad61a64c07b99dee1c8ba
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-11 17:32:34 +0900

    :sparkles: [#52] feat(ReviewController) : 리뷰 전체 조회 컨트롤러 구현

--- src/main/java/com/my/relink/controller/review/ReviewController.java
+++ src/main/java/com/my/relink/controller/review/ReviewController.java
@@ -2,9 +2,12 @@ package com.my.relink.controller.review;
 
 import com.my.relink.config.security.AuthUser;
 import com.my.relink.controller.review.dto.resp.ReviewDetailsRespDto;
+import com.my.relink.controller.review.dto.resp.ReviewPageInfoRespDto;
 import com.my.relink.service.ReviewService;
 import com.my.relink.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,4 +30,15 @@ public class ReviewController {
                 .status(HttpStatus.OK)
                 .body(ApiResult.success(reviewService.findReviewDetailByUserAndReviewId(authUser.getId(), reviewId)));
     }
+
+    @GetMapping("/users/reviews")
+    public ResponseEntity<ApiResult<ReviewPageInfoRespDto>> getReviewList(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PageableDefault(page = 0, size = 10) Pageable pageable
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResult.success(reviewService.findAllReviewByWriterUserId(authUser.getId(), pageable)));
+    }
+
 }
