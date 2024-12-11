43f0cb8885049052601cb340b98d3fbb74685979
siyeon02 <chrysapark@cau.ac.kr>
2024-12-11 17:42:12 +0900

    feat(trade-dto) : 운송장 번호 요청 dto #16

---
+++ src/main/java/com/my/relink/controller/trade/dto/request/TrackingNumberReqDto.java
@@ -0,0 +1,10 @@
+package com.my.relink.controller.trade.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TrackingNumberReqDto {
+    private String trackingNumber;
+}
