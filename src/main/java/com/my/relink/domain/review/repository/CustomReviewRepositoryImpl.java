dd3cbc50d72b3e1d4a5feaca5a867852920af044
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-11 17:31:45 +0900

    :sparkles: [#52] feat(CustomReviewRepository, CustomReviewRepositoryImpl) : 리뷰 전체 조회 Query dsl 구현

--- src/main/java/com/my/relink/domain/review/repository/CustomReviewRepositoryImpl.java
+++ src/main/java/com/my/relink/domain/review/repository/CustomReviewRepositoryImpl.java
@@ -5,14 +5,20 @@ import com.my.relink.domain.image.EntityType;
 import com.my.relink.domain.review.TradeReview;
 import com.my.relink.domain.review.repository.dto.ReviewDetailRepositoryDto;
 import com.my.relink.domain.review.repository.dto.ReviewDetailWithOutTradeReview;
+import com.my.relink.domain.review.repository.dto.ReviewListRepositoryDto;
+import com.my.relink.domain.review.repository.dto.ReviewListWithOutTradeStatusRepositoryDto;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.my.relink.domain.image.QImage.image;
 import static com.my.relink.domain.item.exchange.QExchangeItem.exchangeItem;
@@ -59,6 +65,7 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 
         List<ExchangeItemImageListRespDto> imageList = getExchangeItemImageList(reviewOptional.get().getExchangeItemId());
         List<TradeReview> tradeReviews = getTradeReviews(reviewId);
+
         return Optional.of(new ReviewDetailRepositoryDto(reviewOptional.get(), tradeReviews, imageList));
     }
 
@@ -78,5 +85,53 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository {
                 .where(image.entityId.eq(exchangeId)
                         .and(image.entityType.eq(EntityType.EXCHANGE_ITEM)))
                 .fetch();
+    }
+
+    @Override
+    public Page<ReviewListRepositoryDto> findAllReviews(Long userId, Pageable pageable) {
+        List<ReviewListWithOutTradeStatusRepositoryDto> reviewList = jpaQueryFactory.select(
+                        Projections.constructor(ReviewListWithOutTradeStatusRepositoryDto.class,
+                                review.id,
+                                exchangeItem.name,
+                                review.star,
+                                review.description,
+                                review.createdAt
+                        )
+                )
+                .from(review)
+                .leftJoin(review.exchangeItem, exchangeItem)
+                .where(review.writer.id.eq(userId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(review.createdAt.desc())
+                .fetch();
+
+        JPAQuery<Long> totalCount = jpaQueryFactory.select(review.count())
+                .from(review)
+                .where(review.writer.id.eq(userId));
+
+        List<Long> reviewIdList = reviewList.stream().map(ReviewListWithOutTradeStatusRepositoryDto::getReviewId)
+                .toList();
+
+        Map<Long, List<TradeReview>> reviewIdWithTradeReviews = getTradeReviews(reviewIdList);
+
+        List<ReviewListRepositoryDto> content = reviewList.stream().map(e ->
+                new ReviewListRepositoryDto(e, reviewIdWithTradeReviews.getOrDefault(e.getReviewId(), new ArrayList<>()))
+        ).toList();
+
+        return PageableExecutionUtils.getPage(content, pageable, totalCount::fetchOne);
+    }
+
+    private Map<Long, List<TradeReview>> getTradeReviews(List<Long> reviewIdList) {
+        return reviewIdList.isEmpty() ?
+                new HashMap<>() :
+                em.createQuery("select new map(r.id as reviewId, tr as tradeReview) from Review r join r.tradeReview tr where r.id in :reviewIds order by r.createdAt desc ", Map.class)
+                        .setParameter("reviewIds", reviewIdList)
+                        .getResultList()
+                        .stream()
+                        .collect(Collectors.groupingBy(
+                                m -> (Long) m.get("reviewId"),
+                                Collectors.mapping(m -> (TradeReview) m.get("tradeReview"), Collectors.toList())
+                        ));
     }
 }
