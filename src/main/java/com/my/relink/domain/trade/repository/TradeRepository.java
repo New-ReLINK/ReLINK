803db13a2ef97d1ef6c3b38e79f63339798d72ed
siyeon02 <chrysapark@cau.ac.kr>
2024-12-12 20:43:48 +0900

    refactor(trade-repository) : n+1 방지를 위해 JPQL 리팩토링 #13

--- src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
+++ src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
@@ -1,5 +1,6 @@
 package com.my.relink.domain.trade.repository;
 
+import com.my.relink.domain.image.EntityType;
 import com.my.relink.domain.trade.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,13 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
             "join fetch t.requester " +
             "where t.id = :tradeId")
     Optional<Trade> findByIdWithItemsAndUser(@Param("tradeId") Long tradeId);
+
+    @Query("SELECT t FROM Trade t " +
+            "LEFT JOIN FETCH t.requester r " +
+            "LEFT JOIN FETCH t.requesterExchangeItem rei " +
+            "LEFT JOIN FETCH t.ownerExchangeItem oei " +
+            "LEFT JOIN FETCH Image reiImg ON reiImg.entityId = rei.id AND reiImg.entityType = :entityType " +
+            "LEFT JOIN FETCH Image oeiImg ON oeiImg.entityId = oei.id AND oeiImg.entityType = :entityType " +
+            "WHERE t.id = :tradeId")
+    Optional<Trade> findTradeWithDetails(@Param("tradeId") Long tradeId, @Param("entityType") EntityType entityType);
 }
