174aeebb46dc7642e3f4fc2c513bb162a1344e97
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-12 18:24:51 +0900

    :sparkles: [#101] feat(Repository) : 사용자 신뢰도 지수 Repository 구현

--- src/main/java/com/my/relink/domain/item/exchange/repository/ExchangeItemRepository.java
+++ src/main/java/com/my/relink/domain/item/exchange/repository/ExchangeItemRepository.java
@@ -1,8 +1,10 @@
 package com.my.relink.domain.item.exchange.repository;
 
 import com.my.relink.domain.item.exchange.ExchangeItem;
+import com.my.relink.domain.trade.TradeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeItemRepository extends JpaRepository<ExchangeItem, Long> {
 
+    long countByTradeStatusAndUserId(TradeStatus status, Long userId);
 }
