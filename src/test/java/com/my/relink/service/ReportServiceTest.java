c2cda24afc1cd8bb80219d28c58d5e3860b8a43a
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-12 19:05:22 +0900

    test(report-service): 거래 신고 생성 테스트 케이스 추가 #96 - 동일 신고 대상자에 대해 중복 신고를 요청할 시 예외 발생

--- src/test/java/com/my/relink/service/ReportServiceTest.java
+++ src/test/java/com/my/relink/service/ReportServiceTest.java
@@ -18,6 +18,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -71,6 +74,11 @@ class ReportServiceTest {
             void success() {
                 ArgumentCaptor<Report> reportCaptor = ArgumentCaptor.forClass(Report.class);
                 when(tradeService.findByIdWithUsersOrFail(tradeId)).thenReturn(trade);
+                when(reportRepository.findByEntityIdAndReportTypeAndTargetUserId(
+                        tradeId,
+                        ReportType.TRADE,
+                        partnerId
+                )).thenReturn(Optional.empty());
                 doNothing().when(trade).validateAccess(reporterId);
 
                 reportService.createTradeReport(tradeId, reporterId, reportDto);
@@ -120,6 +128,29 @@ class ReportServiceTest {
                 verify(reportRepository, never()).save(any());
             }
 
+            @Test
+            @DisplayName("동일 신고 대상자에 대해 중복 신고를 요청할 시 예외가 발생한다")
+            void throwsException_whenAlreadyReportedReport(){
+                Report report = Report.builder()
+                        .entityId(tradeId)
+                        .targetUserId(partnerId)
+                        .id(1L)
+                        .build();
+                when(tradeService.findByIdWithUsersOrFail(tradeId)).thenReturn(trade);
+                when(reportRepository.findByEntityIdAndReportTypeAndTargetUserId(
+                        tradeId,
+                        ReportType.TRADE,
+                        partnerId
+                )).thenReturn(Optional.of(report));
+                when(partner.getId()).thenReturn(partnerId);
+                when(trade.getPartner(reporterId)).thenReturn(partner);
+
+                BusinessException exception = assertThrows(BusinessException.class, () ->
+                        reportService.createTradeReport(tradeId, reporterId, reportDto));
+
+                verify(reportRepository, never()).save(any());
+                assertEquals(exception.getErrorCode(), ErrorCode.ALREADY_REPORTED_TRADE);
+            }
 
         }
     }
