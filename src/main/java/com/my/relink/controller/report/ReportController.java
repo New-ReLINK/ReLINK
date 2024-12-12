488a3d75d5012b068daeefb7704c12d6e21355a6
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-12 16:53:42 +0900

    feat(report-controller): 거래 신고하기 dto 추가 #96

--- src/main/java/com/my/relink/controller/report/ReportController.java
+++ src/main/java/com/my/relink/controller/report/ReportController.java
@@ -1,13 +1,16 @@
 package com.my.relink.controller.report;
 
 import com.my.relink.config.security.AuthUser;
+import com.my.relink.controller.report.dto.request.TradeReportCreateReqDto;
 import com.my.relink.service.ReportService;
 import com.my.relink.util.api.ApiResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +21,9 @@ public class ReportController {
 
     @PostMapping("/trades/{tradeId}/report")
     public ResponseEntity<ApiResult<Void>> createTradeReport(@PathVariable("tradeId") Long tradeId,
+                                                             @RequestBody @Valid TradeReportCreateReqDto tradeReportCreateReqDto,
                                                              @AuthenticationPrincipal AuthUser authUser){
-        reportService.createTradeReport(tradeId, authUser.getId());
+        reportService.createTradeReport(tradeId, authUser.getId(), tradeReportCreateReqDto);
         return ResponseEntity.ok(ApiResult.success(null));
     }
 
--- src/main/java/com/my/relink/controller/report/dto/request/TradeReportCreateReqDto.java
+++ src/main/java/com/my/relink/controller/report/dto/request/TradeReportCreateReqDto.java
@@ -1,2 +1,34 @@
-package com.my.relink.controller.report.dto.request;public class TradeReportCreateReqDto {
+package com.my.relink.controller.report.dto.request;
+
+import com.my.relink.common.validation.EnumValidator;
+import com.my.relink.domain.report.Report;
+import com.my.relink.domain.report.ReportReason;
+import com.my.relink.domain.report.ReportType;
+import com.my.relink.domain.trade.Trade;
+import com.my.relink.domain.user.User;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class TradeReportCreateReqDto {
+
+    @EnumValidator(enumClass = ReportReason.class)
+    @NotBlank(message = "신고 사유는 필수입니다")
+    private ReportReason reportReason;
+
+    private String description;
+
+    public Report toEntity(Trade trade, User targetUser){
+        return Report.builder()
+                .reportType(ReportType.TRADE)
+                .entityId(trade.getId())
+                .targetUserId(targetUser.getId())
+                .reportReason(reportReason)
+                .description(description)
+                .build();
+    }
+
+
 }
