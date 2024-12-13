f7c998aecb06eecea04cd24f395ffcf30648111c
siyeon02 <chrysapark@cau.ac.kr>
2024-12-13 17:32:01 +0900

    refactor(trade-dto) : 정적 팩토리 메서드로 dto 변환 #17

--- src/main/java/com/my/relink/controller/trade/dto/response/TradeCancelRespDto.java
+++ src/main/java/com/my/relink/controller/trade/dto/response/TradeCancelRespDto.java
@@ -1,5 +1,9 @@
 package com.my.relink.controller.trade.dto.response;
 
+import com.my.relink.domain.image.Image;
+import com.my.relink.domain.item.exchange.ExchangeItem;
+import com.my.relink.domain.trade.Trade;
+import com.my.relink.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,4 +17,12 @@ public class TradeCancelRespDto {
     private String tradeStartedAt;
     private String partnerExchangeItemImageUrl;
 
+    public static TradeCancelRespDto from(User partner, ExchangeItem partnerExchangeItem, Image partnerExchangeItemImage, String tradeStartedAt){
+        return new TradeCancelRespDto(
+                partnerExchangeItem.getName(),
+                partner.getNickname(),
+                tradeStartedAt,
+                partnerExchangeItemImage != null ? partnerExchangeItemImage.getImageUrl() : null
+        );
+    }
 }
