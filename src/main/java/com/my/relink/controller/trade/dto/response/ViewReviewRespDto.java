e6ab286e1d931021ff97e08068ef6e03da54d6da
siyeon02 <chrysapark@cau.ac.kr>
2024-12-16 14:53:26 +0900

    feat(trade-dto) : 리뷰 조회 응답 dto #108

---
+++ src/main/java/com/my/relink/controller/trade/dto/response/ViewReviewRespDto.java
@@ -0,0 +1,28 @@
+package com.my.relink.controller.trade.dto.response;
+
+import com.my.relink.domain.item.exchange.ExchangeItem;
+import com.my.relink.domain.trade.Trade;
+import com.my.relink.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ViewReviewRespDto {
+    private Long tradeId;
+    private String partnerExchangeItemImage;
+    private String partnerExchangeItemName;
+    private String partnerNickname;
+    private String completedAt;
+
+    public static ViewReviewRespDto from(Trade trade, String partnerExchangeItemImage, User partner, ExchangeItem partnerExchangeItem, String completedAt){
+        return new ViewReviewRespDto(
+                trade.getId(),
+                partnerExchangeItemImage,
+                partnerExchangeItem.getName(),
+                partner.getNickname(),
+                completedAt
+        );
+
+    }
+}
