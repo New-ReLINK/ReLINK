3904ba7e3cb2b7f3d755a79a962a759c3f5f91ec
Geonu Yu <dbrjdn2051@naver.com[C>
2024-12-16 15:43:57 +0900

    :sparkles: [#129] feat(ImageService) : 유저 프로필 등록 서비스 로직 구현

--- src/main/java/com/my/relink/service/ImageService.java
+++ src/main/java/com/my/relink/service/ImageService.java
@@ -1,12 +1,17 @@
 package com.my.relink.service;
 
+import com.my.relink.config.s3.S3Service;
+import com.my.relink.controller.image.dto.resp.ImageUserProfileCreateRespDto;
 import com.my.relink.domain.image.EntityType;
 import com.my.relink.domain.image.Image;
 import com.my.relink.domain.image.repository.ImageRepository;
 import com.my.relink.domain.item.exchange.ExchangeItem;
+import com.my.relink.ex.BusinessException;
+import com.my.relink.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -18,6 +23,7 @@ import java.util.stream.Collectors;
 public class ImageService {
 
     private final ImageRepository imageRepository;
+    private final S3Service s3Service;
 
     public String getExchangeItemUrl(ExchangeItem exchangeItem) {
         return imageRepository.findTopByEntityIdAndEntityTypeOrderByCreatedAtAsc(
@@ -33,4 +39,21 @@ public class ImageService {
                 .collect(Collectors.toMap(Image::getEntityId, Image::getImageUrl));
     }
 
+    public ImageUserProfileCreateRespDto addUserProfile(Long userId, MultipartFile file) {
+        imageRepository.findTopByEntityIdAndEntityTypeOrderByCreatedAtAsc(userId, EntityType.USER)
+                .ifPresent(image -> {
+                    throw new BusinessException(ErrorCode.ALREADY_IMAGE_FILE);
+                });
+
+        String imageUrl = s3Service.upload(file);
+
+        Image image = Image.builder()
+                .imageUrl(imageUrl)
+                .entityType(EntityType.USER)
+                .entityId(userId)
+                .build();
+
+        Image savedImage = imageRepository.save(image);
+        return new ImageUserProfileCreateRespDto(savedImage);
+    }
 }
