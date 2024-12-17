5f41466fa4e3624c37c6c4d398c22f479ba60163
GoUiJeong <uijeong0525@gmail.com>
2024-12-17 10:41:42 +0900

    feat(donation-dto) : 기부상세페이지 dto 추가

---
+++ src/main/java/com/my/relink/controller/donation/dto/resp/DonationItemDetailRespDto.java
@@ -0,0 +1,36 @@
+package com.my.relink.controller.donation.dto.resp;
+
+import com.my.relink.domain.item.donation.DonationItem;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Builder
+public class DonationItemDetailRespDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private String category;
+    private LocalDate createdAt;
+
+    public static DonationItemDetailRespDto fromEntity(DonationItem item,  Map<Long, String> imageMap) {
+        String imageUrl = imageMap.get(item.getId());
+
+        return DonationItemDetailRespDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .category(item.getCategory().getName())
+                .imageUrl(imageUrl)
+                .createdAt(item.getCreatedAt().toLocalDate())
+                .build();
+    }
+}
+
+
+
