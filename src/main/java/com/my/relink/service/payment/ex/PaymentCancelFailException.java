844a1cc875dbdbcaddb60f5f215a49a3aeb63e91
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-17 14:30:40 +0900

    test(payment-service): 결제를 통한 포인트 충전 기능 테스트 케이스 추가 #127

--- src/main/java/com/my/relink/service/payment/ex/PaymentCancelFailException.java
+++ src/main/java/com/my/relink/service/payment/ex/PaymentCancelFailException.java
@@ -1,2 +1,10 @@
-package com.my.relink.service.payment.ex;public class PaymentCancelFailException {
+package com.my.relink.service.payment.ex;
+
+import com.my.relink.ex.BusinessException;
+import com.my.relink.ex.ErrorCode;
+
+public class PaymentCancelFailException extends BusinessException {
+    public PaymentCancelFailException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
