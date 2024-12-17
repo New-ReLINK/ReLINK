1d6e345a4223d1434ee448f58689656056c3f36a
GoUiJeong <uijeong0525@gmail.com>
2024-12-17 17:34:37 +0900

    feat(donation) : - 에러코드 기부상품 찾을수 없음 추가 - repository에 findByIdWithCategory 추가

--- src/main/java/com/my/relink/domain/item/donation/repository/DonationItemRepository.java
+++ src/main/java/com/my/relink/domain/item/donation/repository/DonationItemRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface DonationItemRepository extends JpaRepository<DonationItem, Long> {
 
     @Query("SELECT d FROM DonationItem d " +
@@ -28,4 +30,7 @@ public interface DonationItemRepository extends JpaRepository<DonationItem, Long
     long countCompletedDonationsThisMonth();
 
     Page<DonationItem> findByUserId(Long userId, Pageable pageable);
+
+    @Query("SELECT d FROM DonationItem d JOIN FETCH d.category WHERE d.id = :id")
+    Optional<DonationItem> findByIdWithCategory(@Param("id") Long id);
 }
