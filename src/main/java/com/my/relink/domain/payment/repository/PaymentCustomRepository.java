5a95b7725f60d052ded740d9cc4d720b1b7958e8
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-18 16:38:34 +0900

    (payment-repo): querydsl 포인트 충전 내역 조회 기능 추가 #171

--- src/main/java/com/my/relink/domain/payment/repository/PaymentCustomRepository.java
+++ src/main/java/com/my/relink/domain/payment/repository/PaymentCustomRepository.java
@@ -1,2 +1,10 @@
-package com.my.relink.domain.payment.repository;public interface PaymentCustomRepository {
+package com.my.relink.domain.payment.repository;
+
+import com.my.relink.controller.point.dto.response.PointChargeHistoryRespDto;
+import com.my.relink.domain.user.User;
+import com.my.relink.util.page.PageResponse;
+
+public interface PaymentCustomRepository {
+
+    PageResponse<PointChargeHistoryRespDto> findPointChargeHistories(User user, int page, int size);
 }
--- src/main/java/com/my/relink/domain/payment/repository/PaymentCustomRepositoryImpl.java
+++ src/main/java/com/my/relink/domain/payment/repository/PaymentCustomRepositoryImpl.java
@@ -1,2 +1,71 @@
-package com.my.relink.domain.payment.repository;public class PaymentCustomRepositoryImpl {
+package com.my.relink.domain.payment.repository;
+
+import com.my.relink.controller.point.dto.response.PointChargeHistoryRespDto;
+import com.my.relink.domain.payment.Payment;
+import com.my.relink.domain.payment.PaymentType;
+import com.my.relink.domain.user.User;
+import com.my.relink.util.DateTimeUtil;
+import com.my.relink.util.page.PageInfo;
+import com.my.relink.util.page.PageResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import static com.my.relink.domain.payment.QPayment.*;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentCustomRepositoryImpl implements PaymentCustomRepository{
+
+    private final JPAQueryFactory queryFactory;
+    private final DateTimeUtil dateTimeUtil;
+
+    @Override
+    public PageResponse<PointChargeHistoryRespDto> findPointChargeHistories(User user, int page, int size) {
+        List<Payment> paymentList = queryFactory
+                .selectFrom(payment)
+                .where(
+                        payment.user.eq(user)
+                                .and(payment.paymentType.eq(PaymentType.POINT_CHARGE))
+                )
+                .orderBy(payment.createdAt.desc())
+                .offset(page * size)
+                .limit(size)
+                .fetch();
+
+        List<PointChargeHistoryRespDto> contents = convertToDto(paymentList);
+        PageInfo pageInfo = getPointChargePageInfo(user, page, size);
+
+        return new PageResponse<>(contents, pageInfo);
+    }
+
+    private List<PointChargeHistoryRespDto> convertToDto(List<Payment> paymentList){
+        return paymentList.stream()
+                .map(payment -> new PointChargeHistoryRespDto(
+                        payment,
+                        dateTimeUtil.getUsagePointHistoryFormattedTime(payment.getPaidAt())
+                ))
+                .toList();
+    }
+
+    private PageInfo getPointChargePageInfo(User user, int page, int size){
+        Long totalCount = queryFactory
+                .select(payment.countDistinct())
+                .from(payment)
+                .where(
+                        payment.user.eq(user)
+                        .and(payment.paymentType.eq(PaymentType.POINT_CHARGE))
+                )
+                .fetchOne();
+
+        long count = totalCount != null ? totalCount : 0L;
+        int totalPages = (int) Math.ceil((double) count / size);
+
+        return new PageInfo (
+                totalPages,
+                count,
+                page > 0,
+                page + 1 < totalPages);
+    }
 }
