7ccea2c3bbeb94f1d2cd6ecef9c928cacbcd65bb
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-12-18 15:15:52 +0900

    :twisted_rightwards_arrows: [#147] merge: 메인 파일 추가

--- src/test/java/com/my/relink/service/TradeServiceTest.java
+++ src/test/java/com/my/relink/service/TradeServiceTest.java
@@ -367,8 +367,8 @@ class TradeServiceTest extends DummyObject {
         Mockito.when(userRepository.findById(requester.getId())).thenReturn(Optional.of(requester));
 //        Mockito.when(userRepository.findById(owner.getId())).thenReturn(Optional.of(owner));
         Mockito.when(tradeRepository.findTradeWithDetails(tradeId)).thenReturn(Optional.of(trade));
-        Mockito.when(imageService.getExchangeItemUrl(myExchangeItem)).thenReturn(myImageUrl);
-        Mockito.when(imageService.getExchangeItemUrl(partnerExchangeItem)).thenReturn(partnerImageUrl);
+        Mockito.when(imageService.getExchangeItemThumbnailUrl(myExchangeItem)).thenReturn(myImageUrl);
+        Mockito.when(imageService.getExchangeItemThumbnailUrl(partnerExchangeItem)).thenReturn(partnerImageUrl);
 
 //        Mockito.when(userRepository.findById(trade.getPartner(requester.getId()).getId())).thenReturn(Optional.of(partnerUser));
         Mockito.when(dateTimeUtil.getTradeStatusFormattedTime(trade.getModifiedAt()))
@@ -448,7 +448,7 @@ class TradeServiceTest extends DummyObject {
 
         Mockito.when(userRepository.findById(requester.getId())).thenReturn(Optional.of(requester));
         Mockito.when(tradeRepository.findById(tradeId)).thenReturn(Optional.of(trade));
-        Mockito.when(imageService.getExchangeItemUrl(partnerExchangeItem)).thenReturn("http://example.com/partner-image.jpg");
+        Mockito.when(imageService.getExchangeItemThumbnailUrl(partnerExchangeItem)).thenReturn("http://example.com/partner-image.jpg");
 
         // 서비스 호출
         ViewTradeCancelRespDto result = tradeService.viewCancelTrade(tradeId, new AuthUser(requester.getId(), "test@email.com", Role.USER));
@@ -569,7 +569,7 @@ class TradeServiceTest extends DummyObject {
 
         Mockito.when(userRepository.findById(requester.getId())).thenReturn(Optional.of(requester));
         Mockito.when(tradeRepository.findById(tradeId)).thenReturn(Optional.of(trade));
-        Mockito.when(imageService.getExchangeItemUrl(partnerExchangeItem)).thenReturn(partnerImage);
+        Mockito.when(imageService.getExchangeItemThumbnailUrl(partnerExchangeItem)).thenReturn(partnerImage);
         Mockito.when(dateTimeUtil.getTradeStatusFormattedTime(trade.getModifiedAt()))
                 .thenReturn("2024년 12월 12일 14:30");
 
@@ -583,7 +583,7 @@ class TradeServiceTest extends DummyObject {
         assertEquals("2024년 12월 12일 14:30", result.getCompletedAt());
 
         verify(tradeRepository).findById(tradeId);
-        verify(imageService).getExchangeItemUrl(partnerExchangeItem);
+        verify(imageService).getExchangeItemThumbnailUrl(partnerExchangeItem);
         verify(dateTimeUtil).getTradeStatusFormattedTime(trade.getModifiedAt());
 
     }
