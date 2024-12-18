becfa6544f1d31016df61ef60e8bdb256594d709
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-18 13:42:59 +0900

    chore(gitignore): application ignore 해제 #121

---
+++ src/main/java/com/my/relink/ReLinkApplication.java
@@ -0,0 +1,11 @@
+package com.my.relink;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReLinkApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
