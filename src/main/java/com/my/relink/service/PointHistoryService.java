09e02a67369d64d435e5364436db77838e388ab1
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-18 11:31:50 +0900

    feat(pointhistory-service): 포인트 사용 내역 조회 기능 추가 #121

--- src/main/java/com/my/relink/service/PointHistoryService.java
+++ src/main/java/com/my/relink/service/PointHistoryService.java
@@ -1,2 +1,23 @@
-package com.my.relink.service;public class PointHistoryService {
+package com.my.relink.service;
+
+import com.my.relink.controller.point.dto.response.PointUsageHistoryRespDto;
+import com.my.relink.domain.point.pointHistory.repository.PointHistoryRepository;
+import com.my.relink.domain.user.User;
+import com.my.relink.util.page.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PointHistoryService {
+
+    private final PointHistoryRepository pointHistoryRepository;
+    private final UserService userService;
+
+    public PageResponse<PointUsageHistoryRespDto> getPointUsageHistories(Long userId, int page, int size) {
+        User user = userService.findByIdOrFail(userId);
+        return pointHistoryRepository.findPointUsageHistories(user, page, size);
+    }
 }
