e4b80c555959f0caa9041e8336fa42ce92d2a929
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-18 13:41:32 +0900

    feat(pointhistory-repo): 포인트 사용 내역 조회 쿼리 분리 #121

--- src/main/java/com/my/relink/controller/point/dto/response/PointUsageHistoryRespDto.java
+++ src/main/java/com/my/relink/controller/point/dto/response/PointUsageHistoryRespDto.java
@@ -3,39 +3,37 @@ package com.my.relink.controller.point.dto.response;
 import com.my.relink.domain.trade.TradeStatus;
 import com.my.relink.util.DateTimeUtil;
 import com.my.relink.util.page.PageResponse;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
+@ToString
 public class PointUsageHistoryRespDto {
 
     private Long tradeId;
     private String partnerExchangeItemName;
     private String tradeStatus;
+    @Setter
     private Integer depositAmount;
+    @Setter
     private Integer refundAmount;
+    @Setter
     private String depositDateTime;
+    @Setter
     private String refundDateTime;
-    private LocalDateTime depositLocalDateTime;
-    private LocalDateTime refundLocalDateTime;
+
 
     @Builder
-    public PointUsageHistoryRespDto(Long tradeId, String partnerExchangeItemName, TradeStatus tradeStatus, Integer depositAmount, Integer refundAmount, LocalDateTime depositLocalDateTime, LocalDateTime refundLocalDateTime) {
+    public PointUsageHistoryRespDto(Long tradeId, String partnerExchangeItemName, String tradeStatus, Integer depositAmount, Integer refundAmount, String depositDateTime, String refundDateTime) {
         this.tradeId = tradeId;
         this.partnerExchangeItemName = partnerExchangeItemName;
-        this.tradeStatus = tradeStatus.getMessage();
+        this.tradeStatus = tradeStatus;
         this.depositAmount = depositAmount;
         this.refundAmount = refundAmount;
-        this.depositLocalDateTime= depositLocalDateTime;
-        this.refundLocalDateTime = refundLocalDateTime;
+        this.depositDateTime = depositDateTime;
+        this.refundDateTime = refundDateTime;
     }
 
-    public void formatDateTime(DateTimeUtil dateTimeUtil){
-        this.depositDateTime = dateTimeUtil.getUsagePointHistoryFormattedTime(depositLocalDateTime);
-        this.refundDateTime = dateTimeUtil.getUsagePointHistoryFormattedTime(refundLocalDateTime);
-    }
 }
--- src/main/java/com/my/relink/domain/point/pointHistory/repository/PointHistoryCustomRepositoryImpl.java
+++ src/main/java/com/my/relink/domain/point/pointHistory/repository/PointHistoryCustomRepositoryImpl.java
@@ -4,21 +4,25 @@ import com.my.relink.controller.point.dto.response.PointUsageHistoryRespDto;
 import com.my.relink.domain.point.pointHistory.PointTransactionType;
 import com.my.relink.domain.point.pointHistory.QPointHistory;
 import com.my.relink.domain.trade.QTrade;
+import com.my.relink.domain.trade.TradeStatus;
 import com.my.relink.domain.user.User;
 import com.my.relink.util.DateTimeUtil;
 import com.my.relink.util.page.PageInfo;
 import com.my.relink.util.page.PageResponse;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 
 import static com.my.relink.domain.point.pointHistory.QPointHistory.*;
 import static com.my.relink.domain.trade.QTrade.*;
+import static com.querydsl.jpa.JPAExpressions.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -31,53 +35,71 @@ public class PointHistoryCustomRepositoryImpl implements PointHistoryCustomRepos
         QTrade t = trade;
         QPointHistory ph = pointHistory;
 
-        List<PointUsageHistoryRespDto> contents = queryFactory
-                .select(Projections.constructor(PointUsageHistoryRespDto.class,
+        List<Tuple> tradeInfoList = queryFactory
+                .select(ph.trade.id, ph.createdAt.max())
+                .from(ph)
+                .where(
+                        ph.point.user.eq(user)
+                                .and(ph.pointTransactionType.in(
+                                        PointTransactionType.RETURN,
+                                        PointTransactionType.DEPOSIT
+                                ))
+                )
+                .groupBy(ph.trade.id)
+                .orderBy(ph.createdAt.max().desc())
+                .offset(page * size)
+                .limit(size)
+                .fetch();
+
+        List<Long> tradeIdList = tradeInfoList.stream()
+                .map(tradeInfo -> tradeInfo.get(0, Long.class))
+                .toList();
+
+        List<Tuple> pointHistoryTuple = queryFactory
+                .select(
                         t.id,
+                        t.tradeStatus,
                         new CaseBuilder()
                                 .when(t.requester.eq(user))
                                 .then(t.ownerExchangeItem.name)
                                 .otherwise(t.requesterExchangeItem.name),
-                        t.tradeStatus,
-                        new CaseBuilder()
-                                .when(ph.pointTransactionType.eq(PointTransactionType.DEPOSIT))
-                                .then(ph.amount.multiply(-1))
-                                .otherwise((Integer) null),
-                        new CaseBuilder()
-                                .when(ph.pointTransactionType.eq(PointTransactionType.RETURN))
-                                .then(ph.amount)
-                                .otherwise((Integer) null),
-                        new CaseBuilder()
-                                .when(ph.pointTransactionType.eq(PointTransactionType.DEPOSIT))
-                                .then(ph.createdAt)
-                                .otherwise((LocalDateTime) null),
-                        new CaseBuilder()
-                                .when(ph.pointTransactionType.eq(PointTransactionType.RETURN))
-                                .then(ph.createdAt)
-                                .otherwise((LocalDateTime) null)
-                ))
+                        ph.pointTransactionType,
+                        ph.amount,
+                        ph.createdAt
+                )
                 .from(ph)
                 .join(ph.trade, t)
-                .where(
-                        ph.point.user.eq(user)
-                                .and(ph.pointTransactionType.in(
-                                        PointTransactionType.DEPOSIT,
-                                        PointTransactionType.RETURN
-                                ))
-                ).groupBy(
-                        t.id,
-                        t.tradeStatus,
-                        t.requesterExchangeItem.name,
-                        t.ownerExchangeItem.name,
-                        t.requester.id
-                )
-                .orderBy(ph.createdAt.desc())
-                .offset((long) page * size)
-                .limit(size)
+                .where(t.id.in(tradeIdList))
                 .fetch();
 
+        Map<Long, PointUsageHistoryRespDto> dtoMap = new LinkedHashMap<>();
+        for (Tuple tuple : pointHistoryTuple) {
+            Long tradeId = tuple.get(0, Long.class);
+            TradeStatus tradeStatus = tuple.get(1, TradeStatus.class);
+            String itemName = tuple.get(2, String.class);
+            PointTransactionType type = tuple.get(3, PointTransactionType.class);
+            Integer amount = tuple.get(4, Integer.class);
+            LocalDateTime createdAt = tuple.get(5, LocalDateTime.class);
 
-        contents.forEach(content -> content.formatDateTime(dateTimeUtil));
+            PointUsageHistoryRespDto dto = dtoMap.computeIfAbsent(tradeId, k ->
+                    PointUsageHistoryRespDto.builder()
+                            .tradeId(tradeId)
+                            .tradeStatus(tradeStatus.getMessage())
+                            .partnerExchangeItemName(itemName)
+                            .build());
+
+            if(type == PointTransactionType.DEPOSIT){
+                dto.setDepositAmount(amount * -1);
+                dto.setDepositDateTime(dateTimeUtil.getUsagePointHistoryFormattedTime(createdAt));
+            } else {
+                dto.setRefundAmount(amount);
+                dto.setRefundDateTime(dateTimeUtil.getUsagePointHistoryFormattedTime(createdAt));
+            }
+        }
+
+        List<PointUsageHistoryRespDto> contents = tradeIdList.stream()
+                .map(dtoMap::get)
+                .toList();
 
         Long totalCount = queryFactory
                 .select(ph.trade.countDistinct())
