88bae53d3a27cd00eae280348357af99c1a439fe
siyeon02 <chrysapark@cau.ac.kr>
2024-12-19 14:42:36 +0900

    refactor(trade-repository) : 주석 제거 #186

--- src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
+++ src/main/java/com/my/relink/domain/trade/repository/TradeRepository.java
@@ -22,27 +22,12 @@ public interface TradeRepository extends JpaRepository<Trade, Long>, CustomTrade
             "where t.id = :tradeId")
     Optional<Trade> findByIdWithItemsAndUser(@Param("tradeId") Long tradeId);
 
-//    @Query("SELECT t FROM Trade t " +
-//            "LEFT JOIN FETCH t.requester r " +
-//            "LEFT JOIN FETCH t.requesterExchangeItem rei " +
-//            "LEFT JOIN FETCH t.ownerExchangeItem oei " +
-//            "WHERE t.id = :tradeId")
-//    Optional<Trade> findTradeWithDetails(@Param("tradeId") Long tradeId);
-
     @Query("select t from Trade t " +
             "join fetch t.requester " +
             "join fetch t.ownerExchangeItem oi " +
             "join fetch oi.user " +
             "where t.id = :tradeId")
     Optional<Trade> findByIdWithUsers(@Param("tradeId") Long tradeId);
-
-//    boolean existsByRequesterIdAndTradeStatus(Long userId, TradeStatus tradeStatus);
-
-//    @Query("SELECT t FROM Trade t " +
-//            "JOIN FETCH t.ownerExchangeItem oei " + // 거래의 owner가 등록한 아이템 정보
-//            "JOIN FETCH t.requesterExchangeItem rei " + // 거래의 partner가 등록한 아이템 정보
-//            "WHERE t.id = :tradeId")
-//    Optional<Trade> findByIdWithExchangeItem(@Param("tradeId") Long tradeId);
 
     @Query("SELECT t.id FROM Trade t " +
             "WHERE t.ownerExchangeItem.id = :itemId " +
