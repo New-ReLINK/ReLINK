9332813c11446ed321fa2b09e15ba2100f41f328
ㅇi쀼z1 <yaewon0411@naver.com>
2024-12-19 10:21:47 +0900

    feat(chat-controller): 채팅 이미지 저장 consumes 지정 #178

--- src/main/java/com/my/relink/chat/controller/ChatController.java
+++ src/main/java/com/my/relink/chat/controller/ChatController.java
@@ -6,18 +6,15 @@ import com.my.relink.chat.controller.dto.request.ChatMessageReqDto;
 import com.my.relink.chat.controller.dto.response.ChatImageRespDto;
 import com.my.relink.chat.controller.dto.response.ChatMessageRespDto;
 import com.my.relink.chat.service.ChatService;
-import com.my.relink.config.security.AuthUser;
-import com.my.relink.service.ImageService;
-import com.my.relink.service.MessageService;
 import com.my.relink.util.api.ApiResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +28,6 @@ public class ChatController {
 
     private final SimpMessageSendingOperations messagingTemplate;
     private final ChatService chatService;
-    private final ImageService imageService;
 
     @MessageMapping("/chats/{tradeId}/message")
     public void handleMessage(@DestinationVariable("tradeId") Long tradeId,
@@ -44,10 +40,10 @@ public class ChatController {
         messagingTemplate.convertAndSend("/topic/chats/" + tradeId, response);
     }
 
-    @PostMapping("/chats/{tradeId}/images")
+    @PostMapping(value = "/chats/{tradeId}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResult<ChatImageRespDto>> saveImageForChat(@PathVariable("tradeId") Long tradeId,
                                                                         @ModelAttribute @Valid ChatImageReqDto chatImageReqDto){
-        return ResponseEntity.ok(ApiResult.success(imageService.saveImageForChat(tradeId, chatImageReqDto)));
+        return ResponseEntity.ok(ApiResult.success(chatService.saveImageForChat(tradeId, chatImageReqDto)));
     }
 
 }
