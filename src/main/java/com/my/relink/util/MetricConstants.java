ece05b06b10aa0b01d481658540c5f24df793dcb
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-12-24 18:04:26 +0900

    :sparkles: [#212] feat(MetricConstants) : Metric 관련 상수 추가

---
+++ src/main/java/com/my/relink/util/MetricConstants.java
@@ -0,0 +1,8 @@
+package com.my.relink.util;
+
+
+public class MetricConstants {
+    public static final String SERVICE_USER_TIME = "service.user.time";
+    public static final String SERVICE_TRADE_TIME = "service.trade.time";
+    public static final String SERVICE_EXCHANGE_ITEM_TIME = "service.exchangeItem.time";
+}
