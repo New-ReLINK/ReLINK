1b94907080a838959c7799bdb2657348cf250485
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-12-26 15:12:31 +0900

    :sparkles: [#212] feat(Service) : 메트릭 수집 설정 추가

--- src/main/java/com/my/relink/util/MetricConstants.java
+++ src/main/java/com/my/relink/util/MetricConstants.java
@@ -5,4 +5,7 @@ public class MetricConstants {
     public static final String SERVICE_USER_TIME = "service.user.time";
     public static final String SERVICE_TRADE_TIME = "service.trade.time";
     public static final String SERVICE_EXCHANGE_ITEM_TIME = "service.exchangeItem.time";
+    public static final String SERVICE_DONATION_ITEM_TIME = "service.donationItem.time";
+    public static final String SERVICE_REVIEW_TIME = "service.review.time";
+    public static final String SERVICE_USER_TRUST_SCORE_TIME = "service.userTrustScore.time";
 }
